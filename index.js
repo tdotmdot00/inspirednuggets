656d6cc0e5d696b31d0634ecd473d56dffaf3539
tdotmdot00 <51458672+tdotmdot00@users.noreply.github.com>
2020-02-24 10:31:43 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const SlackBot = require('slackbots');
 const axios = require('axios')
 const dotenv = require('dotenv')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 dotenv.config()
 
